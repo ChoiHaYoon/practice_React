16136f772539163aae40e1f840148cdbf61a6fb6
ChoiHaYoon <alice0763@naver.com>
2021-01-26 20:24:15 +0900

    210126 14강 form따로 빼기

--- react-basic2/src/App.js
+++ react-basic2/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Movie from './component/Movie';
+import MovieForm from './component/MovieForm';
 
 function App() {
   const [movieTitle, setTitle] = useState('');
@@ -17,14 +18,16 @@ function App() {
     );
   });
 
-  const onSubmit = (event) => {
-    event.preventDefault(); // submit될때 refresh가 안되도록
+  const addMovie = (movie) => {
+    // event.preventDefault(); // submit될때 refresh가 안되도록
     // movies.push({ title: movieTitle, year: movieYear});
     // 이렇게 해도 화면에 추가가 안되는 이유 >> state가 작동하지 않기때문에
     // input에 값이들어가는것은 setTitle과 setYear에 값이 들어가는것 >> 즉 변화가 있기 때문에 state가 되고있어 재렌더링 중인것이다.
     // 하지만 버튼을 클릭했을때는 작동이 되는것이 아니기때문에 재렌더링이 되지 않고있다
     // setMovies를 이용해서 추가해야한다
-    setMovies([...movies, {title: movieTitle, year: movieYear}]);
+    // setMovies([...movies, {title: movieTitle, year: movieYear}]);
+    // MovieForm에 movie객체 보내기
+    setMovies([...movies, movie]); // 대괄호에 두개 다 넣어야하는거 까먹지 말기!
     /* 
       전체값을 변경해주는게 아니기 때문에 깊은 복사(...movies)를 해주어서 []<< 대괄호안에 원하는 값을 넣어주는것
       내가 알고있던 [...movies]만 하게되면 기존의 state(여기서는 movies)를 덮어버리게 된다
@@ -43,18 +46,22 @@ function App() {
      
 
     // 값을 입력 후 input안에 들어가있는 useState값을 리셋시켜주기
-    setTitle('');
-    setYear('');
+    // setTitle('');
+    // setYear('');
   }
 
   return (
     <div className="App">
       <h1>Movie List</h1>
+      {/* MovieForm으로 따로 js를 만들어서 import를 해줌
       <form onSubmit={onSubmit}>
         <input type="text" value={movieTitle} placeholder="영화제목" onChange={(e) => setTitle(e.target.value)} /><br/>
         <input type="text" value={movieYear} placeholder="개봉년도" onChange={(e) => setYear(e.target.value)} /><br/>
         <button>영화추가</button>
-      </form>
+      </form> 
+      */}
+      {/* props로 onSubmit함수 보내기 >> addMovie라는 키값으로 */}
+      <MovieForm addMovie={addMovie}/>
       {renderMovies}
     </div>
   );
---
+++ react-basic2/src/component/MovieForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+
+
+// 부모컴포넌트에서 보내는 key값을 {}안에 적어주기
+const MovieForm = ({addMovie}) => {
+    // form만 따로 빼내기
+    const [movieTitle, setTitle] = useState('');
+    const [movieYear, setYear] = useState('');
+
+    const resetForm = () => {
+        setTitle('');
+        setYear('');
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        addMovie({title: movieTitle, year: movieYear});
+        // setMovies([...movies, {title: movieTitle, year: movieYear}]);
+        resetForm();
+    }    
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="text" value={movieTitle} placeholder="영화제목" onChange={(e) => setTitle(e.target.value)} /><br/>
+            <input type="text" value={movieYear} placeholder="개봉년도" onChange={(e) => setYear(e.target.value)} /><br/>
+            <button>영화추가</button>
+        </form>
+    )
+};
+
+export default MovieForm;
